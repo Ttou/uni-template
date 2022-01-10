6f08a4d2a383e0023b6cd1003550ef41b01c823b
WangRuiJin <wangruijin@mrfashion.com>
2022-01-10 16:18:55 +0800

    refactor: 完善代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <view class="content">
+  <view class="page">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
       <text class="title">{{ title }}</text>
       <text class="count">{{ count }}</text>
-      <button @click="handleClick">点击</button>
+      <button class="btn" @click="handleCount">计数</button>
+      <button class="btn" @click="handleOpen">弹窗</button>
     </view>
+    <uni-popup ref="dialogRef">
+      <view class="dialog">
+        <view class="content">
+          <text>Hello World</text>
+        </view>
+      </view>
+    </uni-popup>
   </view>
 </template>
 
@@ -16,55 +24,75 @@ import { useMainStore } from '@/store'
 
 export default defineComponent({
   setup() {
+    const dialogRef = ref<ComponentRef>(null)
     const title = ref('Hello')
 
     const mainStore = useMainStore()
 
     const count = computed(() => mainStore.count)
 
-    function handleClick() {
+    function handleCount() {
       mainStore.add()
     }
 
+    function handleOpen() {
+      dialogRef.value?.open()
+    }
+
     return {
+      dialogRef,
       title,
       count,
-      handleClick
+      handleCount,
+      handleOpen
     }
   }
 })
 </script>
 
-<style scoped>
-.content {
+<style lang="scss" scoped>
+.page {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin: 200rpx auto 50rpx;
+  }
 
-.text-area {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
+  .text-area {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
 
-.count {
-  margin: 20rpx auto;
+  .count {
+    margin: 20rpx auto;
+  }
+
+  .btn + .btn {
+    margin-top: 20rpx;
+  }
+
+  .dialog {
+    .content {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 400rpx;
+      height: 300rpx;
+      background-color: #fff;
+      border-radius: 16rpx;
+    }
+  }
 }
 </style>
