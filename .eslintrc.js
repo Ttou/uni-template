593ea97986b38835f2153806962c59c4dda4a21b
WangRuiJin <wangruijin@mrfashion.com>
2022-04-13 17:41:09 +0800

    chore: 更新配置和依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,14 +9,18 @@ module.exports = defineEslintConfig({
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier'
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended'
   ],
-  plugins: ['simple-import-sort'],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier'],
   rules: {
     'no-undef': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'prettier/prettier': 'error',
     'vue/attribute-hyphenation': 'off',
     'vue/multi-word-component-names': 'off',
     '@typescript-eslint/ban-types': 'off',
