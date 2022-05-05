92ae1c5b97bc190a3b58afab9b348795bba370a7
WangRuiJin <wangruijin@mrfashion.com>
2022-05-05 17:34:27 +0800

    refactor: 完善代码

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { createPinia } from 'pinia'
+import * as Pinia from 'pinia'
 import { createSSRApp } from 'vue'
 
 import App from './App.vue'
 
 export function createApp() {
   const app = createSSRApp(App)
-  const pinia = createPinia()
+  const pinia = Pinia.createPinia()
 
   app.use(pinia)
 
   return {
-    app
+    app,
+    Pinia
   }
 }
