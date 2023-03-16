d18efa4c2b64543db21d628103b7cc214bce0b34
wangruijin <1361572192@qq.com>
2023-03-16 16:10:46 +0800

    chore(custom): 更新依赖

--- .postcssrc.js
+++
@@ -1,8 +0,0 @@
-const { definePostcssConfig } = require('@ttou/define-config')
-
-module.exports = definePostcssConfig({
-  plugins: [
-    require('@dcloudio/uni-cli-shared').uniPostcssPlugin,
-    require('autoprefixer')
-  ]
-})
