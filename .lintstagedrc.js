c0d533dbda867cf41d379c8fbfc4c1122838382b
wangruijin <1361572192@qq.com>
2023-07-27 10:46:09 +0800

    chore(custom): 更新配置和依赖

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const { defineLintStagedConfig } = require('@ttou/define-config')
-
-module.exports = defineLintStagedConfig({
-  '*.{ts,vue}': ['eslint --fix', 'prettier --write'],
-  '*.{css,scss,vue}': [
-    'stylelint --fix --allow-empty-input',
-    'prettier --write'
-  ]
-})
