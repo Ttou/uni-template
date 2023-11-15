0cbca4d1ab1005080166f81b9dfddfedea06162e
wangruijin <1361572192@qq.com>
2023-11-15 10:22:59 +0800

    refactor(custom): 精简组件写法

--- src/components/Welcome/Welcome.vue
+++ src/components/Welcome/Welcome.vue
@@ -5,7 +5,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-import { welcomeProps } from './Welcome'
+import { welcomeProps } from './Welcome.define'
 
 export default defineComponent({
   name: 'Welcome',
